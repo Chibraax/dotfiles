80714b7ba373b0656e4e2eeda6caf22645fab305
Chibraax <alexandrelesieur@hotmail.fr>
2025-10-25 19:19:11 +0200

    modified:   nvim/lazy-lock.json 	modified:   nvim/lazyvim.json 	modified:   nvim/lua/plugins/mason-workaround.lua 	modified:   nvim/lua/plugins/theme.lua

--- nvim/lua/plugins/theme.lua
+++ nvim/lua/plugins/theme.lua
@@ -1,7 +1,6 @@
 return {
   -- Install without configuration
   { "projekt0n/github-nvim-theme" },
-  { "baliestri/aura-theme" },
   { "askfiy/visual_studio_code" },
   { "scottmckendry/cyberdream.nvim" },
   { "EdenEast/nightfox.nvim" },
